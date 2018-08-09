8cb9074c44558feefc81c9f9cc582378752c858e
IAmMrChen <791293535@qq.com>
2018-08-09 13:30:02 +0800

    基础布局

--- laravelshop/bootstrap/helpers.php
+++ laravelshop/bootstrap/helpers.php
@@ -1,4 +1,9 @@
-<?php 
-function test_helper() {
-    return 'OK';
+<?php
+/*
+* 此方法将当前请求的路由名称转化为css列名称
+* 允许我们针对某个页面做样式定制化
+*/
+
+function route_class () {
+	return str_replace('.', '-', Route::currentRouteName());
 }
