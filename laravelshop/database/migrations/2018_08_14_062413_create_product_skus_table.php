cf58085ef63e3915bac70c74ed9008be58ad2047
IAmMrChen <31738199+IAmMrChen@users.noreply.github.com>
2018-08-14 20:23:57 +0800

    用户界面 - 商品收藏

--- laravelshop/database/migrations/2018_08_14_062413_create_product_skus_table.php
+++ laravelshop/database/migrations/2018_08_14_062413_create_product_skus_table.php
@@ -13,16 +13,16 @@ class CreateProductSkusTable extends Migration
      */
     public function up()
     {
-        Schema::create('product_skus', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->string('description');
-            $table->decimal('price', 10, 2);
-            $table->unsignedInteger('stock');
-            $table->unsignedInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->timestamps();
-        });
+        // Schema::create('product_skus', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->string('title');
+        //     $table->string('description');
+        //     $table->decimal('price', 10, 2);
+        //     $table->unsignedInteger('stock');
+        //     $table->unsignedInteger('product_id');
+        //     $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        //     $table->timestamps();
+        // });
     }
 
     /**
