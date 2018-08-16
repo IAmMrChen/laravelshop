6d008336551bfcb698c0a29028e798c4e935b9ea
IAmMrChen <791293535@qq.com>
2018-08-16 15:19:12 +0800

    关闭未支付订单

--- laravelshop/app/Http/Controllers/OrdersController.php
+++ laravelshop/app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\Order;
+use App\Jobs\CloseOrder;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
 use Illuminate\Http\Request;
@@ -11,7 +12,7 @@ use App\Http\Requests\OrderRequest;
 
 class OrdersController extends Controller
 {
-    public function store (OrderRequest $request) {
+    public function store (Request $request) {
         $user  = $request->user();
         // 开启一个数据库事务
         $order = \DB::transaction(function () use ($user, $request) {
@@ -64,6 +65,8 @@ class OrdersController extends Controller
 
             return $order;
         });
+
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
     }
--- laravelshop/app/Models/ProductSku.php
+++ laravelshop/app/Models/ProductSku.php
@@ -25,6 +25,7 @@ class ProductSku extends Model
         if ($amount < 0) {
             throw new InternalException('加库存不可小于0');
         }
+        
         $this->increment('stock', $amount);
     }
 }
