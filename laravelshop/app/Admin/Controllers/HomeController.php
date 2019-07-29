27893b2fe6af73b698f45ffc136d3017c731e9c9
Chen <791293535@qq.com>
2019-07-29 15:57:10 +0800

    '测试'

--- laravelshop/app/Admin/Controllers/HomeController.php
+++ laravelshop/app/Admin/Controllers/HomeController.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Column;
-use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\content;
 use Encore\Admin\Layout\Row;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return Admin::content(function (Content $content) {
+        return Admin::content(function (content $content) {
 
             $content->header('Dashboard');
             $content->description('Description...');
@@ -45,7 +45,4 @@ class HomeController extends Controller
 
     }
 
-    public function testThree () {
-        
-    }
 }
